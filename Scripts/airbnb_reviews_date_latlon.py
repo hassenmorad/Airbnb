f8be4543c495f23b8e1d432a23979f04948c145a
Hassen Morad <hassen.morad@gmail.com>
2020-09-28 15:29:02 -0700

    Add files via upload

--- Scripts/airbnb_reviews_date_latlon.py
+++ Scripts/airbnb_reviews_date_latlon.py
@@ -6,6 +6,7 @@ reviews = pd.read_csv('airbnb_reviews_us_cities.csv')
 listings = pd.read_csv('airbnb_id_latlon.csv')
 
 comb = pd.merge(listings, reviews, on='listing_id', how='right')
+comb.to_csv('airbnb_reviews_date_latlon.csv', index=False)
 
 # 2019-2020 Data
 recent = comb[comb.date > '2019-03-31']
@@ -13,4 +14,4 @@ recent.to_csv('airbnb_reviews_date_latlon_1920.csv', index=False)
 
 # Removing City & State cols to ease conversion to .shp
 drop_loc = comb.drop(['City','State'], axis=1)
-drop_loc.to_csv('airbnb_reviews_date_latlon.csv', index=False)
+drop_loc.to_csv('airbnb_reviews_date_latlon_only.csv', index=False)
